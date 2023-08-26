4a525ac27a015798749813ef4c53cf32267c988f
CoheySaito　 <cowhey3110@gmail.com>
2023-08-27 08:01:12 +0900

    chore:first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { getFizzBuzzValue } from "./getFizzBuzzValue";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const subtract = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Fizz Buzz</h1>
+      <input type="button" value="add" onClick={add} />
+      <input
+        type="button"
+        value="subtract"
+        onClick={subtract}
+        style={{ marginLeft: "1rem" }}
+      />
+      <div style={{ margin: "1rem 0" }}>
+        <span>Current Number:</span>
+        {count === 0 ? null : <span>{count}</span>}
+      </div>
+      <div>
+        <span>Result:</span>
+        <span>{getFizzBuzzValue(count)}</span>
+      </div>
+    </>
   );
 }
 
---
+++ src/getFizzBuzzValue.js
@@ -0,0 +1,15 @@
+export const getFizzBuzzValue = (count) => {
+  if (count === 0) {
+    return "Start";
+  }
+  if (count % 15 === 0) {
+    return "FizzBuzz";
+  }
+  if (count % 3 === 0) {
+    return "Fizz";
+  }
+  if (count % 5 === 0) {
+    return "Buzz";
+  }
+  return count;
+};
